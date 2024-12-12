ef7df1488fa2c7a825b37ff5fc07228df24c2d8a
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-12-12 14:20:06 -0500

    Create Cocktails.js

---
+++ Cocktails.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  Text,
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import styles from './styles';
+import { fetchCocktails } from './api';
+import ConfirmationModal from './ConfirmationModal';
+
+export default function Cocktails() {
+  const [cocktails, setCocktails] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedCocktail, setSelectedCocktail] = useState({});
+
+  useEffect(() => {
+    const getCocktails = async () => {
+      const fetchedCocktails = await fetchCocktails();
+      setCocktails(fetchedCocktails);
+    };
+
+    getCocktails();
+  }, []);
+
+  const handleItemPress = (cocktail) => {
+    setSelectedCocktail(cocktail);
+    setModalVisible(true);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <FlatList
+          data={cocktails}
+          keyExtractor={(item) => item.idDrink.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => handleItemPress(item)}>
+              <Text style={styles.item}>{item.strDrink}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </ScrollView>
+      <ConfirmationModal
+        animationType="fade"
+        visible={modalVisible}
+        cocktail={selectedCocktail}
+        onPressConfirm={() => setModalVisible(false)}
+        onPressCancel={() => setModalVisible(false)}
+      />
+    </View>
+  );
+}
