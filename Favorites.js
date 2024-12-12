5e904b912e8b212ea5e57a180d935f8869406558
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-12-12 14:20:54 -0500

    Create Favorites.js

---
+++ Favorites.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  Text,
+  FlatList,
+  TextInput,
+} from 'react-native';
+import styles from './styles';
+
+export default function Favorites() {
+  
+}
