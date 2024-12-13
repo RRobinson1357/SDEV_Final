a1786e3b42017a8d4dc46ec2ab0e699198c36feb
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-12-13 13:37:03 -0500

    Update Favorites.js

--- Favorites.js
+++ Favorites.js
@@ -1,13 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import {
-  View,
-  Text,
-  FlatList,
-  TextInput,
-} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import styles from './styles';
+import ConfirmationModal from './ConfirmationModal';
 
-export default function Favorites() {
-  
+export default function Favorites({ favorites }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedCocktail, setSelectedCocktail] = useState({});
+
+  const handleItemPress = (cocktail) => {
+    setSelectedCocktail(cocktail);
+    setModalVisible(true);
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+      <FlatList
+        data={favorites}
+        keyExtractor={(item) => item.idDrink.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handleItemPress(item)}>
+            <Text style={styles.item}>{item.strDrink}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      </ScrollView>
+      <ConfirmationModal
+        animationType="fade"
+        visible={modalVisible}
+        cocktail={selectedCocktail}
+        onPressConfirm={() => setModalVisible(false)}
+        onPressCancel={() => setModalVisible(false)}
+      />
+    </View>
+  );
 }
