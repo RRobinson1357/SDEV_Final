b2191b25e30e7029ec6c268a56e31c918ef65ea0
RRobinson1357 <124370097+RRobinson1357@users.noreply.github.com>
2024-12-13 13:36:10 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,43 @@
+import React, { useState } from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Platform } from "react-native";
 import Home from "./Home";
 import Cocktails from "./Cocktails";
-import Favorites from "./Favorites"
+import Favorites from "./Favorites";
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
 export default function App() {
+  const [favorites, setFavorites] = useState([]);
+
   return (
     <NavigationContainer>
       {Platform.OS === "ios" && (
         <Tab.Navigator>
           <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Cocktails" component={Cocktails} />
-          <Tab.Screen name="Favorites" component={Favorites} />
+          <Tab.Screen name="Cocktails">
+            {() => <Cocktails favorites={favorites} setFavorites={setFavorites} />}
+          </Tab.Screen>
+          <Tab.Screen name="Favorites">
+            {() => <Favorites favorites={favorites} />}
+          </Tab.Screen>
         </Tab.Navigator>
       )}
 
       {Platform.OS == "android" && (
         <Drawer.Navigator>
           <Drawer.Screen name="Home" component={Home} />
-          <Drawer.Screen name="Cocktails" component={Cocktails} />
-          <Drawer.Screen name="Favorites" component={Favorites} />
+          <Drawer.Screen name="Cocktails">
+            {() => <Cocktails favorites={favorites} setFavorites={setFavorites} />}
+          </Drawer.Screen>
+          <Drawer.Screen name="Favorites">
+            {() => <Favorites favorites={favorites} />}
+          </Drawer.Screen>
         </Drawer.Navigator>
       )}
     </NavigationContainer>
   );
 }
-
